aa9338273e28c6196ab5e2f2f68a9ec69efde802
michee <nangmamichee0@gmail.com>
2024-05-08 22:00:59 +0100

    manipulation des données JSON

--- main.go
+++ main.go
@@ -1,21 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"mics/functions"
 )
 
-func calculvaleur(intChan chan int) {
-	randomNombre := functions.RandomValue(50)
-	intChan <- randomNombre
+type User struct {
+	Id    int    `json:"id"`
+	Nom   string `json:"nom"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
 }
- 
+
 func main() {
-	fmt.Println("cool")
-	random := make(chan int)
+	jsonApi := `
+		[ 
+			{
+				"id": 1,
+				"nom": "michée",
+				"age": 20,
+				"email": "mic@gmail.com"
+			},
+			{
+				"id": 2,
+				"nom": "michou",
+				"age": 21,
+				"email": "mics@gmail.com"
+			}
+		]
+	`
 
-	go calculvaleur(random)
+	var users []User
 
-	 num := <- random
-	 fmt.Println(num) 
+	err := json.Unmarshal([]byte(jsonApi), &users)
+
+	if err != nil {
+		fmt.Println("Erreur json", err)
+	}
+
+	fmt.Printf("Données json %v\n", users)
+
+	var monStruct []User
+	var user_1 User
+	user_1.Age = 2
+	user_1.Email = "mon@gmqil.com"
+	user_1.Id = 3
+	user_1.Nom = "Mon"
+
+	monStruct = append(monStruct, user_1)
+
+	jsonSlice, err := json.MarshalIndent(monStruct, "", " ")
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	fmt.Println(string(jsonSlice))
 }
