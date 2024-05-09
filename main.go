e8b4ff58869005f51fdc45e79aaaec7e3105bfbd
michee <nangmamichee0@gmail.com>
2024-05-09 01:33:05 +0100

    tester les codes

--- main.go
+++ main.go
@@ -1,58 +1,23 @@
 package main
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
 )
 
-type User struct {
-	Id    int    `json:"id"`
-	Nom   string `json:"nom"`
-	Age   int    `json:"age"`
-	Email string `json:"email"`
+func division(a, b float32) (float32, error) {
+	if b == 0 {
+		return 0, errors.New("impossible de diviser par 0")
+	}
+
+	return a / b, nil
 }
 
 func main() {
-	jsonApi := `
-		[ 
-			{
-				"id": 1,
-				"nom": "michée",
-				"age": 20,
-				"email": "mic@gmail.com"
-			},
-			{
-				"id": 2,
-				"nom": "michou",
-				"age": 21,
-				"email": "mics@gmail.com"
-			}
-		]
-	`
-
-	var users []User
-
-	err := json.Unmarshal([]byte(jsonApi), &users)
-
+	resultat, err := division(5, 2)
 	if err != nil {
-		fmt.Println("Erreur json", err)
+		panic(err)
 	}
 
-	fmt.Printf("Données json %v\n", users)
-
-	var monStruct []User
-	var user_1 User
-	user_1.Age = 2
-	user_1.Email = "mon@gmqil.com"
-	user_1.Id = 3
-	user_1.Nom = "Mon"
-
-	monStruct = append(monStruct, user_1)
-
-	jsonSlice, err := json.MarshalIndent(monStruct, "", " ")
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	fmt.Println(string(jsonSlice))
+	fmt.Println(resultat)
 }
